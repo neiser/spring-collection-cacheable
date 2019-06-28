a65e203522bc6f4c44bc98dc490063adb53bbe7f
Andreas Grub <andreas.grub@qaware.de>
2019-06-28 09:16:27 +0200

    Initial commit from start.spring.io

---
+++ src/main/java/com/example/springcollectioncacheable/SpringCollectionCacheableApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcollectioncacheable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCollectionCacheableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCollectionCacheableApplication.class, args);
+	}
+
+}
